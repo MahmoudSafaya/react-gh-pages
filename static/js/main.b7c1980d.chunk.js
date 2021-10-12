2202b3701c7232a46dc2bf2a541587a88bd1a6be
MahmoudSafaya <mahmoudsafaya7@gmail.com>
2021-10-12 15:28:58 +0200

    Updates

---
+++ static/js/main.b7c1980d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpe-app"]=this["webpackJsonpe-app"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),i=t(3),s=t.n(i),o=(t(8),t(9),t(0));var p=function(){return Object(o.jsx)("div",{className:"App",children:"Hello Friend"})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(p,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.b7c1980d.chunk.js.map
